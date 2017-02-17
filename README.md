919e3e6c9b1d9ab5d7ffa000129a503dddb55869
Caoimhe Chaos <caoimhechaos@protonmail.com>
2017-02-17 06:47:38 +0100

    Create a prometheus exporter for per-process statistics.

---
+++ README.md
@@ -0,0 +1,14 @@
+Prometheus Process Exporter
+===========================
+
+The Prometheus Process Exporter picks up stats from all processes running
+in the system (or container) and exports them as prometheus variables.
+
+Currently, the following stats are exported:
+
+ - node_os_process_memory: size of the process.
+
+The stats are updated once per minute. Each of the variables, unless
+specified otherwise, is exported as a map with the process name as
+the key. Multiple processes of the same type will be aggregated by
+adding up their stats.
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	"net/http"
+
+	"github.com/mitchellh/go-ps"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var procTotalMem = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Namespace: "node",
+		Subsystem: "os",
+		Name:      "process_memory",
+		Help:      "Number of bytes allocated by process name (sum of all matching)",
+	},
+	[]string{"procname"})
+
+func calculateMemory(pid int) (uint64, error) {
+	f, err := os.Open(fmt.Sprintf("/proc/%d/smaps", pid))
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	res := uint64(0)
+	pfx := []byte("Pss:")
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		line := r.Bytes()
+		if bytes.HasPrefix(line, pfx) {
+			var size uint64
+			_, err := fmt.Sscanf(string(line[4:]), "%d", &size)
+			if err != nil {
+				return 0, err
+			}
+			res += size
+		}
+	}
+	if err := r.Err(); err != nil {
+		return 0, err
+	}
+
+	return res, nil
+}
+
+func updateProcessCounters() {
+	var procSizes = make(map[string]uint64)
+
+	procs, err := ps.Processes()
+
+	if err != nil {
+		log.Print("Error fetching process list: ", err)
+		return
+	}
+
+	for _, proc := range procs {
+		size, err := calculateMemory(proc.Pid())
+		if err == nil {
+			procSizes[proc.Executable()] += size
+		} else {
+			log.Print("Error calculating memory for ", proc.Executable(), ": ",
+				err)
+		}
+	}
+
+	for name, size := range procSizes {
+		procTotalMem.WithLabelValues(name).Set(float64(size))
+	}
+}
+
+func processTicker(t <-chan time.Time) {
+	for range t {
+		updateProcessCounters()
+	}
+}
+
+func main() {
+	var listen string
+	var port int
+
+	flag.StringVar(&listen, "listen", "",
+		"Host name to bind to")
+	flag.IntVar(&port, "port", 0,
+		"Assign specific port to the listener")
+	flag.Parse()
+
+	prometheus.MustRegister(procTotalMem)
+
+	go processTicker(time.Tick(1 * time.Minute))
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(
+		net.JoinHostPort(listen, strconv.Itoa(port)), nil))
+}
